15ced97abf74509c225d3c2e5454d352667c7dac
Lorenz Lechner <sppedflyer@gmail.com>
2024-08-25 14:56:41 +0200

    python3_executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cmake -G "Ninja" -B build -S . \
       -D CMAKE_BUILD_TYPE="Release" \
       -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-      -D Python_EXECUTABLE="$PYTHON" \
+      -D Python3_EXECUTABLE="$PYTHON" \
       -D PASTIX_ORDERING_SCOTCH:BOOL=OFF
 
 ninja -C build install
