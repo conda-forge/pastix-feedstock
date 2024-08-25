e4040080068aefeacd8962cdd85c9facaf76cc9e
Lorenz Lechner <sppedflyer@gmail.com>
2024-08-25 20:43:30 +0200

    split packages into noarch and library

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -5,9 +5,10 @@ cmake -G "Ninja" -B build -S . \
       -D CMAKE_BUILD_TYPE="Release" \
       -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
+      -D Python3_EXECUTABLE="$PYTHON" \
       -D PASTIX_ORDERING_SCOTCH:BOOL=OFF \
-      -D BUILD_WRAPPERS:BOOL=OFF \
-      -D BUILD_LIBS:BOOL=ON
+      -D BUILD_WRAPPERS:BOOL=ON \
+      -D BUILD_LIBS:BOOL=OFF
 
 ninja -C build install
 
