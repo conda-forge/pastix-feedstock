f84a1799f78a10901db33874060bdf453593ca98
Lorenz Lechner <sppedflyer@gmail.com>
2024-08-27 21:54:16 +0200

    BUILD_WRAPPERS->BUILD_PYTHON

--- recipe/build_libpastix.sh
+++ recipe/build_libpastix.sh
@@ -6,7 +6,7 @@ cmake -G "Ninja" -B build -S . \
       -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D PASTIX_ORDERING_SCOTCH:BOOL=OFF \
-      -D BUILD_WRAPPERS:BOOL=OFF \
+      -D BUILD_PYTHON:BOOL=OFF \
       -D BUILD_LIBS:BOOL=ON
 
 ninja -C build install
